e1e3f18e2c326a4a41c4da523511b1ad00e9ad32
Forkeh <forkeh@gmail.com>
2025-04-28 14:49:59 +0200

    chore: moved wiki sidebar to own file

---
+++ Hovedopgave/client/src/app/features/dashboard/pages/wiki/WikiSideMenu.tsx
@@ -0,0 +1,44 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+
+type Props = {
+    onSelectWikiEntry: (id: string) => void;
+};
+
+export default function WikiSideMenu({
+    onSelectWikiEntry: handleWikiEntry,
+}: Props) {
+    return (
+        <nav className='flex w-64 flex-col bg-gray-800 p-3 text-white'>
+            <Button variant='secondary'>Create entry</Button>
+            <Accordion
+                type='single'
+                collapsible
+            >
+                <AccordionItem value='item-1'>
+                    <AccordionTrigger>NPC's</AccordionTrigger>
+                    <AccordionContent>
+                        <div onClick={() => handleWikiEntry('1')}>Entry 1</div>
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value='item-2'>
+                    <AccordionTrigger>Locations</AccordionTrigger>
+                    <AccordionContent>
+                        <div onClick={() => handleWikiEntry('2')}>Entry 1</div>
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value='item-3'>
+                    <AccordionTrigger>Lore</AccordionTrigger>
+                    <AccordionContent>
+                        <div onClick={() => handleWikiEntry('3')}>Entry 1</div>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </nav>
+    );
+}
